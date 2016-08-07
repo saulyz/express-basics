3b3a156884c3c3a69947b8589cbe792611573b83
Saulius Vikerta <saulius.vikerta@gmail.com>
2016-08-07 12:57:49 +0300

    Server tunning

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(request, response){
-	response.send("Working server");
+app.get('/', function(req, res){
+	res.send("<h1>Working server</h1>");
 });
 
-app.listen(3000);
+app.listen(3000, function(){
+	console.log("Frontend server is running on port 3000!");
+});
